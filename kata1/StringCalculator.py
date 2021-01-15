a6a9dc11722b6263c58f6c9a06405ab10da52b16
Francisco de Assis Boldt <fboldt@gmail.com>
2021-01-15 15:18:49 -0300

    kata1 dirty finished

--- kata1/StringCalculator.py
+++ kata1/StringCalculator.py
@@ -1,24 +1,43 @@
 class StringCalculator:
-    @staticmethod
-    def add(numbers: str) -> int:
+    def __init__(self) -> None:
+        super().__init__()
+        self.calledCount = 0
+
+    def getCalledCount(self) -> int:
+        return self.calledCount
+    
+    def add(self, numbers: str) -> int:
+        self.calledCount += 1
+        # set delimiters
         delimiters = [',','\n']
         if numbers.startswith('//'):
-            delimiter, numbers = numbers.split('\n', 1)
-            delimiters.append(delimiter[2:])
-        
+            delimiterstr, numbers = numbers.split('\n', 1)
+            delimiterlist = delimiterstr[2:].split("][")
+            for delimiter in delimiterlist:
+                delimiter = delimiter.replace('[','')
+                delimiter = delimiter.replace(']','')
+                delimiters.append(delimiter)
+        # split numbers
         listOfNumbers = [numbers]
         for delimiter in delimiters:
             tmplist = []
             for numlist in listOfNumbers:
                 tmplist += numlist.split(delimiter)
             listOfNumbers = tmplist
-        
+        # string to int
         result = 0
+        negatives = []
         for number in listOfNumbers:
             if number.strip() != "":
                 numint = int(number)
-                if numint >= 0:
+                if numint < 0:
+                    negatives.append(numint)
+                elif numint <= 1000:
                     result += numint
-                else:
-                    raise Exception("negatives not allowed")
+        # raise exception for negative values
+        if len(negatives)>0:
+            msg = "negatives not allowed:"
+            for value in negatives:
+                msg += " " + str(value)
+            raise Exception(msg)
         return result
--- kata1/test_kata1.py
+++ kata1/test_kata1.py
@@ -35,4 +35,25 @@ def test_negative_number(): #5. exception "negatives not allowed"
 def test_multiple_negatives(): #6. show multiple negatives in exception message
     with pytest.raises(Exception) as excinfo:
         StringCalculator().add("-1,-2")
-    assert "negatives not allowed: -1, -2" in str(excinfo.value)
+    assert "negatives not allowed: -1 -2" in str(excinfo.value)
+
+@pytest.mark.parametrize("calls, result", [
+    ([], 0), ([""], 1), (["",""], 2), 
+])
+def test_getCalledCount(calls, result): #7. how many times the method add has been called
+    sc = StringCalculator()
+    for call in calls:
+        sc.add(call)
+    assert sc.getCalledCount() == result
+
+def test_ignore_big_numbers(): #9. numbers bigger than 100 are ignored
+    assert StringCalculator().add("2,1001") == 2
+
+def test_any_length_delimiter(): #10. format: //[delimiter]\n
+    assert StringCalculator().add("//[***]\n1***2***3") == 6
+
+def test_multiple_delimiters(): #11. format: //[delim1][delim2]\n
+    assert StringCalculator().add("//[*][%]\n1*2%3") == 6
+
+def test_multiple_any_longer_delimiters(): #12. format: //[**][%%]\n1**2%%3
+    assert StringCalculator().add("//[**][%%]\n1**2%%3") == 6
